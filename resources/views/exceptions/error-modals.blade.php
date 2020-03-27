eb34754826fb9cade326dd3524e7d5fad745e077
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-03-27 14:20:44 +0700

    Make dynamic modals for BE error

---
+++ resources/views/exceptions/error-modals.blade.php
@@ -0,0 +1,15 @@
+<div id="error-modal-id" class="ui basic modal">
+    <div class="ui icon header red">
+        <i class="times icon"></i>
+        {{ $title }}
+    </div>
+    <div class="content">
+        <p>{{ $message }}</p>
+    </div>
+    <div class="actions">
+        <div class="ui green ok inverted button">
+            <i class="checkmark icon"></i>
+            Okay
+        </div>
+    </div>
+</div>
