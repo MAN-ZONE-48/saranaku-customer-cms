5c4bbc98630a02108eab67a98af8a3828902a35b
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-03-28 00:37:27 +0700

    Make a save current page on session system

--- app/Http/Controllers/TemplatePageController.php
+++ app/Http/Controllers/TemplatePageController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Impl\TemplatePageControllerImpl;
 
 use Illuminate\Http\Request;
+use Session;
+use App\Entity\ResponseDTO;
 
 class TemplatePageController extends Controller implements TemplatePageControllerImpl
 {
@@ -12,5 +14,13 @@ class TemplatePageController extends Controller implements TemplatePageControlle
         return view('exceptions/error-modals')
                 ->with('title', $request->get('title'))
                 ->with('message', $request->get('message'));
+    }
+
+    public function saveCurrentPage(Request $request){
+        Session::forget('current_page');
+        Session::put('current_page', $request->get('uri'));
+
+        $dto = new ResponseDTO("success", 200, "Current page session updated!", Session::get('current_page'));
+        return json_encode($dto);
     }
 }
