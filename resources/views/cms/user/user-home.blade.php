1971c691efd69a571660150c2b82176cddf40271
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-03-27 14:20:33 +0700

    Created User Home page

---
+++ resources/views/cms/user/user-home.blade.php
@@ -0,0 +1,66 @@
+<section class="ui grid">
+    <div class="doubling four column row">
+        <div class="column">
+            <a class="">
+            <div class="ui centered card">
+                <div class="content">
+                    <div class="center aligned header">
+                        <h1><i class="user plus icon"></i></h1>
+                    </div>
+                    
+                </div>
+                <div class="content">
+                    <div class="center aligned header">
+                        Add User
+                    </div>
+                </div>
+            </div>
+            </a>
+        </div>
+        <div class="column">
+            <div class="ui card">
+                <div class="content">
+                    <div class="center aligned header">
+                        <h1><i class="user icon"></i></h1>
+                    </div>
+                    
+                </div>
+                <div class="content">
+                    <div class="center aligned header">
+                        User List
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="ui card">
+                <div class="content">
+                    <div class="center aligned header">
+                        <h1><i class="user icon"></i></h1>
+                    </div>
+                    
+                </div>
+                <div class="content">
+                    <div class="center aligned header">
+                        Add Role
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="ui card">
+                <div class="content">
+                    <div class="center aligned header">
+                        <h1><i class="user icon"></i></h1>
+                    </div>
+                    
+                </div>
+                <div class="content">
+                    <div class="center aligned header">
+                        Role List
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
