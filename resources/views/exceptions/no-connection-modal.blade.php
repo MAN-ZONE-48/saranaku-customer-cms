0a409a7d0129621039c2103718fae2b1c24908c3
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-03-27 14:20:55 +0700

    Make error modals for no connection

---
+++ resources/views/exceptions/no-connection-modal.blade.php
@@ -0,0 +1,15 @@
+<div id="error-modal-no-connection" class="ui basic modal">
+    <div class="ui icon header red">
+        <i class="unlink icon"></i>
+        No Connection
+    </div>
+    <div class="content">
+        <p>Please check your internet connection.</p>
+    </div>
+    <div class="actions">
+        <div class="ui green ok inverted button">
+            <i class="checkmark icon"></i>
+            Okay
+        </div>
+    </div>
+</div>
