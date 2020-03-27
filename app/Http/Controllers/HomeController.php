fbb708a3948a223d651a4e215f2af766c9455c84
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-03-28 00:36:51 +0700

    Page Session saving System

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Impl\HomeControllerImpl;
 
+use Session;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller implements HomeControllerImpl
 {
     public function getPage(){
-        return view('template/master');
+        $currentPage = Session::get('current_page');
+
+        return view('template/master')
+                ->with("currentPage", $currentPage);
     }
 
     public function getHomePage(){
