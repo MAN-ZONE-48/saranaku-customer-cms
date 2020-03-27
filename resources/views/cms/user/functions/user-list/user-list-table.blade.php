142e0289e50b18bdd8c45373fa4bad213c1b3346
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-03-28 00:40:06 +0700

    Add user list and it's components

---
+++ resources/views/cms/user/functions/user-list/user-list-table.blade.php
@@ -0,0 +1,50 @@
+<tbody class="center aligned">
+    <tr>
+        <td data-label="Fullname">Made Raja Adi</td>
+        <td data-label="Username">dema90</td>
+        <td data-label="Email">maderajaadi@gmail.com</td>
+        <td data-label="Phone">Co-Owner</td>
+        <td data-label="Detail">
+            <div class="ui icon buttons">
+                <button class="ui teal icon button">
+                    <i class="eye icon"></i>
+                </button>
+                <button class="ui red icon button">
+                    <i class="trash alternate icon"></i>
+                </button>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td data-label="Fullname">Wisnu Bimantoro</td>
+        <td data-label="Username">dualital69</td>
+        <td data-label="Email">dualitalexe@gmail.com</td>
+        <td data-label="Phone">Co-Owner</td>
+        <td data-label="Detail">
+            <div class="ui icon buttons">
+                <button class="ui teal icon button">
+                    <i class="eye icon"></i>
+                </button>
+                <button class="ui red icon button">
+                    <i class="trash alternate icon"></i>
+                </button>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td data-label="Fullname">Yosua Kristianto</td>
+        <td data-label="Username">CuaMcCarsaree754</td>
+        <td data-label="Email">yosua_kristianto144@outlook.com</td>
+        <td data-label="Phone">Owner</td>
+        <td data-label="Detail">
+            <div class="ui icon buttons">
+                <button class="ui teal icon button">
+                    <i class="eye icon"></i>
+                </button>
+                <button class="ui red icon button">
+                    <i class="trash alternate icon"></i>
+                </button>
+            </div>
+        </td>
+    </tr>
+</tbody>
---
+++ resources/views/cms/user/pages/user-list.blade.php
@@ -0,0 +1,60 @@
+{{-- Header Page --}}
+<section class="ui grid segment">
+    <div class="doubling one column row left aligned">
+        <div class="column">
+            <h2 class="ui header">
+                <i class="user icon"></i>
+                <div class="content">
+                        User List 
+                    <div class="sub header">
+                        Edit, and manage all users that has been linked on your shops.
+                    </div>
+                </div>
+            </h2>
+        </div>
+    </div>
+</section>
+
+<section class="ui grid segment">
+    <div class="doubling one column row center aligned">
+        <div class="column">
+            <div class="ui grid segment">
+                <div class="seven wide column">
+                    <select class="ui fluid dropdown">
+                        <option value="">Filter</option>
+                        <option value="1">Fullname</option>
+                        <option value="0">Username</option>
+                        <option value="0">Email</option>
+                    </select>
+                </div>
+                    
+                <div class="nine wide column">
+                    <div class="ui icon fluid input">
+                        <input type="text" name="" placeholder="Search...">
+                        <i class="inverted circular search link icon"></i>
+                    </div>
+                </div>
+            </div>
+
+            <table class="ui celled striped table">
+                <thead>
+                    <tr class="center aligned">
+                        <th>Fullname</th>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Detail</th>
+                    </tr>
+                </thead>
+                @include('cms/user/functions/user-list/user-list-table')
+            </table>
+        </div>
+    </div>
+</section>
+
+{{-- @include('cms/user/functions/user-list/delete-user')
+
+
+<script type="text/javascript">
+    $('#hwe').modal('show');
+</script> --}}
