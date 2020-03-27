d09dd838ab82a364f417cbdb2e581e8b467b7af6
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-03-28 00:39:44 +0700

    Added delete user modal

---
+++ resources/views/cms/user/functions/user-list/delete-user.blade.php
@@ -0,0 +1,23 @@
+<div id="hwe" class="ui tiny modal">
+    <i class="close icon"></i>
+    <div class="header">
+        Delete Confirmation
+    </div>
+    <div class="content">
+        <div class="description">
+            <div class="ui header">Are you sure to delete this user?</div>
+            <p>
+                <b>Remember!</b> This action cannot be undone!
+            </p>
+        </div>
+    </div>
+    <div class="actions">
+        <div class="ui black deny button">
+            Cancel
+        </div>
+        <div class="ui negative right labeled icon button">
+            Delete Anyway
+            <i class="trash alternate icon"></i>
+        </div>
+    </div>
+</div>
