87268f0e82379ecf5458a2f7c22bfa4abdde97ac
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-04-03 16:14:40 +0700

    Added user detail page

---
+++ resources/views/cms/user/pages/user-detail.blade.php
@@ -0,0 +1,45 @@
+{{-- Back Button --}}
+<section class="ui grid segment">
+    <div class="doubling one column row">
+        <div class="column">
+            <button class="ui primary basic button" onclick="getPageWithoutToggle(USERLIST);">
+                <i class="arrow left icon"></i> Back
+            </button>
+        </div>
+    </div>
+</section>
+
+{{-- Header Page --}}
+<section class="ui grid segment">
+    <div class="doubling one column row">
+        <div class="column">
+            <h2 class="ui header">
+                <i class="user icon"></i>
+                <div class="content">
+                        Made Raja Adi 
+                    <div class="sub header">
+                       Co-Owner
+                    </div>
+                </div>
+            </h2>
+        </div>
+    </div>
+</section>
+
+{{-- Content --}}
+<section class="ui grid segment">
+    <div class="doubling four column row">
+        <div class="column">
+            Username
+        </div>
+        <div class="column">
+            Email
+        </div>
+        <div class="column">
+            Phone
+        </div>
+        <div class="column">
+            
+        </div>
+    </div>
+</section>
