6f609e639f4bb4914209a7c30ff56518e8443081
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-04-03 16:14:13 +0700

    Added role list table

---
+++ resources/views/cms/user/functions/role-list/role-list-table.blade.php
@@ -0,0 +1,36 @@
+<tbody class="center aligned">
+    <tr>
+        <td data-label="No">1</td>
+        <td data-label="Role Name">Owner</td>
+        <td data-label="Created At">03-04-2020</td>
+        <td data-label="Action">
+            <div class="ui icon buttons">
+                <a onclick="">
+                    <button class="ui teal icon button" title="Edit Role">
+                        <i class="edit icon"></i>
+                    </button>
+                </a>
+                <button class="ui red icon button">
+                    <i class="trash alternate icon"></i>
+                </button>
+            </div>
+        </td>
+    </tr>
+    <tr>
+        <td data-label="No">2</td>
+        <td data-label="Role Name">Co-Owner</td>
+        <td data-label="Created At">03-04-2020</td>
+        <td data-label="Action">
+            <div class="ui icon buttons">
+                <a onclick="">
+                    <button class="ui teal icon button" title="Edit Role">
+                        <i class="edit icon"></i>
+                    </button>
+                </a>
+                <button class="ui red icon button">
+                    <i class="trash alternate icon"></i>
+                </button>
+            </div>
+        </td>
+    </tr>
+</tbody>
