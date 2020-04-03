f157d400a517ef01b9e6cf6cb4ff577ea5a38855
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-04-03 16:12:54 +0700

    Added all Cruds for Stock

--- app/Http/Controllers/Impl/StockControllerImpl.php
+++ app/Http/Controllers/Impl/StockControllerImpl.php
@@ -1,9 +1,30 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers\Impl;
 use Illuminate\Http\Request;
 
 //Buat ngatur Stock
 interface StockControllerImpl{
+    public function getPage();
+    public function createStockPage();
+    public function createInventoryTypePage();
+    public function stockListPage();
+    public function inventoryTypePage();
+    public function updateStockPage(Request $request);
 
+    //Function
+    //PUT
+    public function updateStock(Request $request);
+    public function updateInventoryType(Request $request);
+
+    //POST
+    public function createStock(Request $request);
+    public function createInventoryType(Request $request);
+
+    //DELETE
+    public function deleteStock(Request $request);
+    public function deleteInventoryType(Request $request);
+
+    //PATCH
+    public function addStockQuantity(Request $request);
 }
