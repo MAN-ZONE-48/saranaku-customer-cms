28782f741fa23f61e306548632b81db7d7f51473
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-04-03 16:14:03 +0700

    Added CRUD view for Role

---
+++ resources/views/cms/user/functions/role-list/add-role-modal.blade.php
@@ -0,0 +1,23 @@
+<div id="add-new-role-modal" class="ui modal">
+    <i class="close icon"></i>
+    <div class="header">
+        Add New Role
+    </div>
+    <div class="content">
+        <div class="ui form">
+            <div class="field">
+                <label>Role Name</label>
+                <input type="text" name="lalalala" placeholder="Input Rolename" />
+            </div>
+        </div>
+    </div>
+    <div class="actions">
+        <div class="ui black deny button">
+            Cancel
+        </div>
+        <div class="ui positive right labeled icon button">
+            Create Role
+            <i class="check icon"></i>
+        </div>
+    </div>
+</div>
---
+++ resources/views/cms/user/functions/role-list/delete-role-modal.blade.php
@@ -0,0 +1,23 @@
+<div id="delete-role-modal" class="ui tiny modal">
+    <i class="close icon"></i>
+    <div class="header">
+        Delete Confirmation
+    </div>
+    <div class="content">
+        <div class="description">
+            <div class="ui header">Are you sure to delete this role?</div>
+            <p>
+                <b>Remember!</b> This action cannot be undone!
+            </p>
+        </div>
+    </div>
+    <div class="actions">
+        <div class="ui black deny button">
+            Cancel
+        </div>
+        <div class="ui negative right labeled icon button">
+            Delete Anyway
+            <i class="trash alternate icon"></i>
+        </div>
+    </div>
+</div>
