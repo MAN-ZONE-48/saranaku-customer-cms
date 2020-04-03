0a85219478302e6bf7fe9a38a299cc12a1a0e6d9
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-04-03 16:12:10 +0700

    Added function to update settings

--- app/Http/Controllers/Impl/SettingsControllerImpl.php
+++ app/Http/Controllers/Impl/SettingsControllerImpl.php
@@ -1,8 +1,13 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers\Impl;
 use Illuminate\Http\Request;
 
 interface SettingsControllerImpl{
-    
+    //Page
+    public function getPage();
+
+    //Settings
+    public function updateSetting(Request $request);
+
 }
