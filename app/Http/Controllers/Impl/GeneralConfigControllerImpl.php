6caa000fd682d53734050d8383f0c877c4423a70
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-04-03 16:12:39 +0700

    Added IMPL for GCO

---
+++ app/Http/Controllers/Impl/GeneralConfigControllerImpl.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Impl;
+use Illuminate\Http\Request;
+
+interface GeneralConfigControllerImpl{
+
+
+    //Settings
+    public function updateSetting(Request $request);
+
+}
