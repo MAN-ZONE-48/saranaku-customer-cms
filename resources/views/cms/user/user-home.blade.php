7ac85dad2f84dcb99183e6c92f424e29b7c61919
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-04-03 16:13:50 +0700

    Changing User Home

--- resources/views/cms/user/user-home.blade.php
+++ resources/views/cms/user/user-home.blade.php
@@ -1,30 +1,13 @@
 <section class="ui grid">
-    <div class="doubling four column row">
+    <div class="doubling two column row center aligned">
         <div class="column">
-            <a class="">
-            <div class="ui centered card">
-                <div class="content">
-                    <div class="center aligned header">
-                        <h1><i class="user plus icon"></i></h1>
-                    </div>
-                    
-                </div>
-                <div class="content">
-                    <div class="center aligned header">
-                        Add User
-                    </div>
-                </div>
-            </div>
-            </a>
-        </div>
-        <div class="column">
-            <a onclick="getPageAjax(USERLIST);">
+            <a onclick="getPageWithoutToggle(USERLIST);">
             <div class="ui card">
                 <div class="content">
                     <div class="center aligned header">
                         <h1><i class="user icon"></i></h1>
                     </div>
-                    
+
                 </div>
                 <div class="content">
                     <div class="center aligned header">
@@ -34,35 +17,23 @@
             </div>
             </a>
         </div>
+
         <div class="column">
-            <div class="ui card">
-                <div class="content">
-                    <div class="center aligned header">
-                        <h1><i class="user icon"></i></h1>
+            <a onclick="getPageWithoutToggle(ROLELIST);">
+                <div class="ui card">
+                    <div class="content">
+                        <div class="center aligned header">
+                            <h1><i class="user icon"></i></h1>
+                        </div>
+
                     </div>
-                    
-                </div>
-                <div class="content">
-                    <div class="center aligned header">
-                        Add Role
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="column">
-            <div class="ui card">
-                <div class="content">
-                    <div class="center aligned header">
-                        <h1><i class="user icon"></i></h1>
-                    </div>
-                    
-                </div>
-                <div class="content">
-                    <div class="center aligned header">
-                        Role List
+                    <div class="content">
+                        <div class="center aligned header">
+                            Role List
+                        </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </div>
     </div>
 </section>
