c24da7f21c929f4fc7f6c938f9445a4f6776c7cf
Yosua Kristianto <yosua.kristianto@indosystem.com>
2020-04-03 16:13:05 +0700

    Added  USER related url

--- public/js/custom-scripts/url-list.js
+++ public/js/custom-scripts/url-list.js
@@ -2,7 +2,15 @@ const TRANSACTION = "/transaction-management/";
 const CASHIER = "/transaction-management/cashier";
 
 //User Management
-const USER = "/user-management/";
-const USERLIST = "/user-management/list";
+    //User
+    const USER = "/user-management/";
+    const USERLIST = "/user-management/list";
+    const USERDETAIL = "/user-management/detail";
+
+    //Role
+    const ROLELIST = "/role-management/list";
+    const ROLEDETAIL = "/role-management/detail";
+    const ROLEDELETE = "/role-management/delete";
+    const ROLEUPDATE = "/role-management/update";
 
 const HOME = "/cms/home/";
